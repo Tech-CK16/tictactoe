e0208da7dddf54f33cf05be73ba1d5435b069ba7
chethan <tech.ck16@gmail.com>
2024-08-19 17:52:32 +0530

    tic tac toe

---
+++ src/components/Square.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+type ISquareProps = {
+    onClick: () => void;
+    value: 'X' | 'O' | null;
+    winner: string | null;
+};
+const Square: React.FC<ISquareProps> = ({ onClick, value, winner }) => {
+    if (!value) {
+        return (
+            <button
+                className="board-square"
+                onClick={onClick}
+                disabled={Boolean(winner)}
+            >
+                {value}
+            </button>
+        );
+    }
+    return (
+        <button
+            className={`board-square square_${value.toLowerCase()}`}
+            disabled
+        >
+            {value}
+        </button>
+    );
+};
+
+export default Square;
